6657a90844b12eef9e18db929b053322b8acad27
HangNT169 <HangNT169@fpt.edu.vn>
2023-05-31 10:08:09 +0700

    B11: Dat lich

---
+++ src/main/java/com/poly/hangnt169/B10_DatLich/TestSchedule.java
@@ -0,0 +1,29 @@
+package com.poly.hangnt169.B10_DatLich;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+public class TestSchedule {
+
+    @Autowired
+    private TestService testService;
+
+    // https://crontab.guru/
+    // https://hocspringboot.net/2020/12/26/spring-batch-la-gi/
+    // https://hocspringboot.net/2020/10/29/cron-job-la-gi/
+    // https://www.baeldung.com/spring-scheduled-tasks
+    // Set thoi gian ham service chay
+//    @Scheduled(fixedRate = 2000)  // Tinh theo ms
+    @Scheduled(cron = "30 9 15 * *")
+    public void datChuongBaoThuc() {
+        try {
+            testService.chuongBaoThuc();
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
+}
---
+++ src/main/java/com/poly/hangnt169/B10_DatLich/TestService.java
@@ -0,0 +1,11 @@
+package com.poly.hangnt169.B10_DatLich;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public void chuongBaoThuc(){
+        System.out.println("Day diiii=.===");
+    }
+}
